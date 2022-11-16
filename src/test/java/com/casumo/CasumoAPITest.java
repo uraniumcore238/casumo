e147126c681a9359c82ba6ce747ecc89d77088f6
SergeiT <uraniumcore238@gmail.com>
2022-11-16 20:04:53 +0200

    added some parameters for test

--- src/test/java/com/casumo/CasumoAPITest.java
+++ src/test/java/com/casumo/CasumoAPITest.java
@@ -1,6 +1,6 @@
 package com.casumo;
 
-import static io.qameta.allure.Allure.step;
+
 import static io.restassured.path.xml.XmlPath.CompatibilityMode.HTML;
 import static io.restassured.RestAssured.given;
 
@@ -19,11 +19,8 @@ public class CasumoAPITest extends TestBase {
 
 
     @CsvSource(value = {
-            "input                              | npt",
-            "task                               | tsk",
-            "abcdefghijklmnopqrstuvwxyz         | bcdfghjklmnpqrstvwxz",
+//            "abcdefghijklmnopqrstuvwxyz         | bcdfghjklmnpqrstvwxz",
             "!abc!                              | !bc!",
-            "@#$%^&*()_+=-*<>,.?/{}[];'\"abc~`  | @#$%^&*()_+=-*<>,.?/{}[];'\"bc~`",
             "#abc                               | #bc",
             "@abc                               | @bc",
             "^&*()_+=-*<>,.{}[];'\"abc~`        | ^&*()_+=-*<>,.{}[];'\"bc~`",
@@ -39,8 +36,12 @@ public class CasumoAPITest extends TestBase {
             "{abc}                              | {bc}",
             ";'abc;'                            | ;'bc;'",
             ":ab:c                              | :b:c",
-            "ab\":c                             | b\"c"
-
+            "ab\":c                             | b\"c",
+            "aа                                 | а",
+            "<script>alert<script>              | <scrpt>lrt<scrpt>",
+            "sb?da                              | sb?d",
+            "0123456789abc0123456789            | 0123456789bc0123456789",
+            "abcabc/>                           | bcbc"
     }, delimiter = '|')
     @Owner("Example owner")
     @Story("Check symbols delition")
@@ -65,4 +66,33 @@ public class CasumoAPITest extends TestBase {
 
     }
 
+
+    @CsvSource(value = {
+            "<script>alert</script>             | nullErrorNot Found",
+            "/abcabc>                           | nullErrorNot Found"
+
+
+    }, delimiter = '|')
+    @Owner("Example owner")
+    @Story("Check symbols delition")
+    @DisplayName("To get error response")
+    @ParameterizedTest(name="Test for request {0} and expected response {1}")
+    void getTextWithoutTheVowelsNegativeTest(String req, String resp) {
+
+        Response response = given()
+                .log().uri()
+                .log().body()
+                .contentType(ContentType.HTML)
+                .when()
+                .get("/"+req)
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(STATUS_CODE_404)
+                .extract().response();
+
+        XmlPath htmlPath = new XmlPath(HTML, response.getBody().asString());
+        Assertions.assertEquals(resp, htmlPath.getString("body"));
+
+    }
 }
--- src/test/java/com/casumo/TestBase.java
+++ src/test/java/com/casumo/TestBase.java
@@ -7,6 +7,7 @@ public class TestBase {
     public final int STATUS_CODE_200 = 200;
     public final int STATUS_CODE_201 = 201;
     public final int STATUS_CODE_400 = 400;
+    public final int STATUS_CODE_404 = 404;
     public final int STATUS_CODE_500 = 500;
 
     @BeforeAll
